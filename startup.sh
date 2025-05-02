fdf6e01781395e6b234378922ea1f98f5debf601
unknown <remiko1995@gmail.com>
2025-05-02 06:11:53 -0500

    Fix PDF export with wkhtmltopdf for Azure Linux

--- startup.sh
+++ startup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Instalar dependencias para wkhtmltopdf
-apt-get update
-apt-get install -y fontconfig xfonts-base xfonts-75dpi curl
+# Dar permisos de ejecución al binario wkhtmltopdf
+chmod 755 /home/site/wwwroot/wkhtmltopdf
 
-# Descargar wkhtmltopdf 0.12.6 versión estática (compatible con Azure Linux)
-curl -L -o wkhtmltox.tar.xz https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.amd64.tar.xz
+# Agregar la ruta al PATH
+export PATH=$PATH:/home/site/wwwroot
 
-# Extraer y mover el ejecutable
-tar -xf wkhtmltox.tar.xz
-cp wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
-chmod +x /usr/local/bin/wkhtmltopdf
-
-# Iniciar tu app Flask con Gunicorn
-exec gunicorn app:app
+# Iniciar la aplicación
+gunicorn app:app --bind=0.0.0.0 --timeout 300
